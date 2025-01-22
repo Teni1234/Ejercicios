b7439ee5c7fb5b586e3196a160d53840440bb8d2
Teni1234 <samuelvacagonzalez@gmail.com>
2025-01-22 12:52:30 +0100

    Print

--- ejercicios.py
+++ ejercicios.py
@@ -1,1 +1,3 @@
-print("Hola mundo")
+print("Hola mundo" , " Hola Tierra" , sep= " -" ) #Separa por lo que pongas en las comillas y end hace un salto de linea por defecto
+nombre="Juan"
+print(nombre)
